f8de470eb99c399ff8f92f96703f616613a12100
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-10-03 15:34:59 -0500

    2.1.- Configuramos el servidor, Crear un servidor basico

---
+++ 002 Configurar Servidor NodeJS/2.1 Crear un servidor basico/servidor.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http.createServer(function (request, respuesta){
+    respuesta.writeHead(200, {'Content-Type': 'text/plain'})
+    respuesta.write("hola mundo");
+    respuesta.end();
+}).listen(3000, 'localhost');
+
+console.log('El servidor se muestra en http://localhost:3000');
+
