ea69a164d9f3e9d3a029ada85ad2e27649fe429a
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-10-04 13:17:10 -0500

    3.4.- Crear un sitio web, instalacion de Pug

---
+++ 003 Crear un sitio web/3.4 Instalacion pug/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var pug = require('pug');
+
+/**
+cada vez que se desee buscar un archivo lo hara en esta ruta
+*/
+app.use (express.static(__dirname + '/public') );
+
+app.get('/' , function( peticion, respuesta){
+    respuesta.render('index.pug')
+ })
+
+
+ app.listen( 3000 , function(){
+     console.log("escuchando en el puerto 3000");
+ })
