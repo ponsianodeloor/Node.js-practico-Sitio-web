a335e0e32665b1229675ec4e1d3278f9c76ba919
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-10-04 14:50:31 -0500

    3.6.- Crear un sitio web, datos dinamicos

--- 003 Crear un sitio web/3.4 Instalacion pug/app.js
+++ 003 Crear un sitio web/3.4 Instalacion pug/app.js
@@ -9,7 +9,10 @@ cada vez que se desee buscar un archivo lo hara en esta ruta
 app.use (express.static(__dirname + '/public') );
 
 app.get('/' , function( peticion, respuesta){
-    respuesta.render('index.pug')
+    respuesta.render('index.pug', {
+        titulo: 'Test',
+        textoParrafo: 'Este es un parrafo de prueba'
+    });
  })
 
 
