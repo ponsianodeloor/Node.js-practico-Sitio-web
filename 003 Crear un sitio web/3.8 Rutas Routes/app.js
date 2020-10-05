899d7d5b3637b8fac0e3085f44767dba1fc9ffa9
Ponsiano De Loor <ponsianodeloor@gmail.com>
2020-10-05 00:21:27 -0500

    3.7.- Crear un sitio web, Crear rutas avanzadas

---
+++ 003 Crear un sitio web/3.8 Rutas Routes/app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+
+var pug = require('pug');
+
+/**
+cada vez que se desee buscar un archivo lo hara en esta ruta
+*/
+app.use (express.static(__dirname + '/public') );
+
+app.get('/' , function( peticion, respuesta){
+    respuesta.render('index.pug', {
+        titulo: 'Test',
+        textoParrafo: 'Este es un parrafo de prueba'
+    });
+ });
+
+var lst_camisetas = [
+    {color:'Naranja', imagen:'/images/camiseta1.png'},
+    {color:'Rojo', imagen:'/images/camiseta2.png'},
+    {color:'Azul', imagen:'/images/camiseta3.png'},
+    {color:'Verde', imagen:'/images/camiseta4.png'}
+];
+
+app.get('/tienda' , function( peticion, respuesta){
+respuesta.render('tienda.pug', { camisetas : lst_camisetas });
+});
+
+//app.get('/tienda/comprar/:color/:talla/' , function( peticion, respuesta){
+app.get('/tienda/comprar/:color', function( peticion, respuesta){
+    let datos_de_camiseta = lst_camisetas.filter( function(item){
+        if (peticion.params.color == item.color) {
+            return item;
+        }
+    })[0]
+    respuesta.render('tienda_detalle.pug', { 
+        color : peticion.params.color,
+        camiseta: datos_de_camiseta
+    });
+    });
+
+
+ app.listen( 3000 , function(){
+     console.log("escuchando en el puerto 3000");
+ })
